a684d06c10607b2485414d4f84c4a4c6fcda4a27
Kishore Kumar <kishorekrishnaj5@gmail.com>
2023-07-21 21:37:26 +0530

    Correction Done. Final Codes

--- jalaAcademy/src/testCases/LogOut_TestCase.java
+++ jalaAcademy/src/testCases/LogOut_TestCase.java
@@ -1,11 +1,13 @@
 package testCases;
 
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 import common_Functions.CommonFunctions;
 import pageObjects.LogOut_Page;
 
 public class LogOut_TestCase extends CommonFunctions{
+	@Test
 	public void logout() {
 		PageFactory.initElements(driver, LogOut_Page.class);
 		LogOut_Page.logout.click();
